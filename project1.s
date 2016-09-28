716eb883524227a907517d389022cf62c930c582
Lucas Sanders <lucasbsanders@gmail.com>
2016-09-28 19:56:38 -0400

    Update project1.s

--- project1.s
+++ project1.s
@@ -4,16 +4,18 @@
 
 #data section
 .data
+.align 0
 A: .word 89,19,91,-5,23,-67,31,46,-71,-14,-10,3,67,17,11,-18,43,-73		#load array A with its values
 B: .word 0,0,0,0,0,0,0,0,0,0,0		#load array B with its values
 C: .word 0,0,0,0,0,0,0 		#load array C with its values
-msg0	:	.asciiz "Index of the smallest positive number: %d\n""
-msg1	:	.asciiz "Array B:"
-msg2	:	.asciiz "Array C:"
-msg3	:	.asciiz " %d"
-msg4	:	.asciiz "\n"
+msg0:	.asciiz "Index of the smallest positive number: %d\n""
+msg1:	.asciiz "Array B:"
+msg2:	.asciiz "Array C:"
+msg3:	.asciiz " %d"
+msg4:	.asciiz "\n"
 
 .text
+.align 2
 .globl main
 main:
 	la $s0,A		#load register s0 with address of array A
@@ -36,7 +38,7 @@ LOOP_0				#for (i = 0; i < 18; i++)
 	slt $t6,$t5,$t0		#load a temporary value (t6) with 1 if A[i] is less than min
 	slti $t5,$t5,$0		#load a temporary value (t5) with 1 if A[i] is greater than 0
 	and $t5,$t5,$t6		#AND value (t5) with the result of the two previous boolean statements
-	bne $t5,$1,IF_1		#skip the steps in the "if" statement if t5 (boolean within if statement) is not equal to one
+	bne $t5,$1,IF_1		#skip the steps in the if statement if t5 (boolean within if statement) is not equal to one
 				#if( A[i] < min && A[i]>0 )
 	lw $t0,$t2($s0)		#sets min equal to A[i];
 	li $t1,$t2		#sets minIndex equal to i;
@@ -46,9 +48,9 @@ IF_1				#skip to here if the if statement fails
 	slti $t6,$t2,18 	#set temp value t6 to 1, if i is less than 18
 	beq $1,$t6,LOOP_0		#if t6 is 1, then loop back to the start of LOOP_0
 	
-	li $v0, msg0            #print the following: ("Index of the smallest positive number: %d\n", minIndex)
-	addi $a0,$t1,0		#insert the minIndex into the printed statement
-	syscall
+	la $a0,msg0		#print the following: ("Index of the smallest positive number: %d\n", minIndex)
+	mov $a1,$t1		#insert the minIndex into the printed statement
+	jal printf
 	
 #/*Transfer all positive numbers to array B and all negative numbers to array C*/
 
@@ -73,39 +75,39 @@ IF_2				#else statement
 	slti $t6,$t2,18 	#set t6 equal to 1 if i is less than 18
 	beq $1,$t6,LOOP_1		#if t6 is 1, then loop back to the start of LOOP_1
 	
-	li $v0, msg1		#print the following: ("Array B:")
-	syscall
+	la $a0, msg1		#print the following: ("Array B:")
+	jal printf
 	
 	li $t2,$0		#t2 is i, used as an iterator for this loop
 LOOP_2			 	#for (i = 0; i < 11; i++){
-	li $v0,msg3		#print the following: (" %d", B[i])
 	lw $t5,$t2($s1)		#load a temporary value (t5) with B[i]
-	addi $a0,$t5,0		#insert that temporary value into the print statement
-	syscall
+	la $a0,msg3		#print the following: (" %d", B[i])
+	mov $a1,$t5		#insert that temporary value into the print statement
+	jal printf
 	
 	addi $t2,1		#iterate (i plus 1)
 	slti $t6,$t2,11 	#set t6 equal to 1 if i is less than 11
 	beq $1,$t6,LOOP_2		#if t6 is 1, then loop back to the start of LOOP_2
 	
-	li $v0, msg4            #print the following: ("\n:")
-	syscall
+	la $a0, msg4            #print the following: ("\n:")
+	jal printf
 	
-	li $v0, msg2            #print the following: ("Array C:")
-	syscall
+	la $a0, msg2            #print the following: ("Array C:")
+	jal printf
 	
 	li $t2,$0		#t2 is i, used as an iterator for this loop
 LOOP_3			 	#for (i = 0; i < 7; i++)
-	li $v0, msg3            #print the following: (" %d", C[i])
 	lw $t5,$t2($s2)		#load a temporary value (t5) with C[i]
-	addi $a0, $t5, 0	#insert that temporary value into the print statement
-	syscall
+	la $a0,msg3		#print the following: (" %d", C[i])
+	mov $a1,$t5		#insert that temporary value into the print statement
+	jal printf
 	
 	addi $t2,1		#iterate (i plus 1)
 	slti $t6,$t2,7 		#set t6 equal to 1 if i is less than 7
 	beq $1,$t6,LOOP_2		#if t6 is 1, then loop back to the start of LOOP_3 
 
-	li $v0, msg4            #print the following: ("\n:")
-	syscall
+	la $a0, msg4		#print the following: ("\n:")
+	jal printf
 	
 	li $t2,$0		#load t2 with value i=0 
 	
